c0ecdfa2e39e3dbc2453295fb4618ee87818aa1d
Henrik Singmann <singmann@gmail.com>
2018-01-10 19:33:47 +0100

    added sahpe to plots

--- 4_summary_plots.R
+++ 4_summary_plots.R
@@ -3,16 +3,16 @@ library("ggplot2")
 
 dd <- position_dodge(w = .5)
 unnest(results, est_group) %>%
-  ggplot(aes(y = est, x = parameter, col=method)) +
+  ggplot(aes(y = est, x = parameter, col=method, shape = method)) +
   facet_grid(.~condition) +
-  geom_errorbar(aes(ymin = ci_0.025, ymax = ci_0.975), position = dd)+
+  geom_errorbar(aes(ymin = est-se, ymax = est+se), position = dd)+
   geom_point(position = dd) + ylim(0,1) + 
   theme_bw()
 ggsave("plots/estimates.pdf", h = 4.5, w = 8)
 
 
 unnest(results, test_between) %>%
-  ggplot(aes(y = est_diff, x = parameter, col=method)) +
+  ggplot(aes(y = est_diff, x = parameter, col=method, shape = method)) +
   facet_grid(condition2~condition1) +
   geom_errorbar(aes(ymin = ci_0.025, ymax = ci_0.975), position = dd)+
   geom_point(position = dd) + ylim(-1,1) + 
