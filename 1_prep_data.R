f49fbc6948e0e3184c007b5a951155df982e56f0
Daniel Heck <danheck@web.de>
2018-01-11 10:45:00 +0100

    cleaned main script

--- 1_prep_data.R
+++ 1_prep_data.R
@@ -28,8 +28,6 @@ head(data)
 COL_CONDITION <- "ExpCond"
 COL_ID <- "Subject"
 
-#data <- data[c(1,25),]
-
 # NOTE: experimental condition should be labeled meaningfully!
 unique(data[,COL_CONDITION])
 data[,COL_CONDITION] <- factor(data[,COL_CONDITION],
@@ -54,32 +52,12 @@ MAX_CI_INDIV <- 0.99
 ## DO NOT CHANGE CODE BELOW ##
 ##############################
 
-################################### data structure  ##########
-
-# COPIED TO mpt_XXX functions
-
-## frequency columns are read from the EQN file and are alphabetically
-## ordered within each tree! That is undocumented MPTinR behavior.
-# col_freq <- get_eqn_categories(EQN_FILE)
-#c(3:5, 7, 6, 8, 11, 9, 10)
-#colnames(data)[column_freq]
-# data$id <- data[,COL_ID]
-# data$condition <- data[,COL_CONDITION]
-# data_list <- split(data[,c("id", "condition")], f = data[,COL_CONDITION])
-# freq_list <- split(data[,col_freq], f = data[,COL_CONDITION])
-
-
-
-# for testing
+# ### different settings for testing (faster, but inaccurate results)
 # MPTINR_OPTIONS <- c(bootstrap_samples = 10, n.optim = 2, nCPU = 8)
 # TREEBUGS_MCMC <- c(n.chain = 4, n.iter = 2000, n.adapt = 100,
 #                    n.burnin = 100, n.thin = 5,
 #                    Rhat_max = 5, Neff_min = 5,
 #                    n.PPP = 100, nCPU = 8)
-# res_test <- mpt_treebugs(dataset = DATA_FILE, data, model = EQN_FILE, method = "simple", 
-#                            col_id = COL_ID, col_condition = COL_CONDITION)
-
-######### run
 
 res_mptinr <- mpt_mptinr(dataset = DATA_FILE, data = data, model = EQN_FILE,
                          col_id = COL_ID, col_condition = COL_CONDITION)
@@ -88,6 +66,6 @@ res_treebugs <- lapply(c("simple", "simple_pooling", "trait", "beta"),
                        dataset = DATA_FILE, data = data, model = EQN_FILE,
                        col_id = COL_ID, col_condition = COL_CONDITION)
 
-results <- bind_rows(res_mptinr, res_treebugs) #res_simple, res_trait, res_beta)
+results <- bind_rows(res_mptinr, res_treebugs)
 results
 save(results, file = paste0(EQN_FILE, "-", DATA_FILE, ".RData"))
