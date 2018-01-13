f9b5143ed9669ebb19883f9ee07a2b347342ca4e
Daniel Heck <danheck@web.de>
2018-01-13 15:30:37 +0100

    major refactoring of folder structure

--- jaeger2012/1_main_script.R
+++ jaeger2012/1_main_script.R
@@ -2,19 +2,17 @@
 ## Install developer version of TreeBUGS   ##
 #############################################
 
-## * cf. installation instructions: 
-##   - https://github.com/denis-arnold/TreeBUGS 
-##
-## * on Windows this requires to install "Rtools" for building C++ packages:
-##   - https://cran.r-project.org/bin/windows/Rtools/     
-##
-## * on Mac this requires "Xcode Command Line Tools"
-##   - https://www.maketecheasier.com/install-command-line-tools-without-xcode/
-##   - it might be necessary to install the library "gfortran" manually:
-##        curl -O http://r.research.att.com/libs/gfortran-4.8.2-darwin13.tar.bz2
-##        sudo tar fvxz gfortran-4.8.2-darwin13.tar.bz2 -C /  
+### the newest TreeBUGS needs to be installed once using one of the following 3 options:
 
-## uncomment and run the following three lines once to install dependencies:
+### (A) on Windows:
+# install.packages(c("runjags", "hypergeo","rjags", "logspline","RcppArmadillo"))
+# install.packages("../scripts/TreeBUGS_1.2.0.zip", repos = NULL)
+
+### (B) on Mac:
+# install.packages(c("runjags", "hypergeo","rjags", "logspline","RcppArmadillo"))
+# install.packages("../scripts/TreeBUGS_1.2.0.XXX", repos = NULL)
+
+### (C) compile source package (requires tools for building C++ packages)
 # install.packages(c("devtools", "runjags", "hypergeo","rjags", "logspline","RcppArmadillo"))
 # library("devtools")
 # devtools::install_github("denis-arnold/TreeBUGS")
@@ -35,31 +33,34 @@ library("parallel")
 library("MPTinR")
 library("TreeBUGS")
 library("runjags")
+runjags.options(silent.jags = TRUE, silent.runjags = TRUE)
 
 # load scripts. make sure that working directory is correct via either:
 # - setwd() / getwd()
-# Rstudio->Sessions->Set Working Directory->'To Source File Location'
-source("2_mptinr_part.R")
-source("3_treebugs_part.R")
-source("9_auxiliary_functions.R")
-
-# suppress output of JAGS sampler
-runjags.options(silent.jags = TRUE, silent.runjags = TRUE)
+# - Rstudio->Sessions->Set Working Directory->'To Source File Location'
+source("../scripts/mptinr.R")
+source("../scripts/treebugs.R")
+source("../scripts/auxiliary_functions.R")
+source("../scripts/summary_plots.R")
 
 
 #################################
 ## MPT model definition & Data ##
 #################################
 
-EQN_FILE <- "2HTSM_Submodel4.eqn"
-DATA_FILE <- "Kuhlmann_dl7.csv"  
+EQN_FILE <- "2htm.eqn"
+DATA_FILE <- "Jaeger_2012.csv"  
 
 
 ### if .csv format uses semicolons ";" (German format)
-data <- read.csv2(DATA_FILE, fileEncoding = "UTF-8-BOM")
+data <- read.csv(DATA_FILE, fileEncoding = "UTF-8-BOM")
 ### if .csv format uses commata "," (international format)
 # data <- read.csv(DATA_FILE, fileEncoding = "UTF-8-BOM")
 head(data)
+plotFreq(data, boxplot = FALSE, eqn = "2htm.eqn")
+
+### outliers:
+data <- data[-c(35,28),]
 
 
 ### optional: add a person identifier if missing in data
@@ -68,14 +69,14 @@ head(data)
 # data$ExpCond <- "no_condition"
 
 
-COL_ID <- "Subject"         # name of the variable encoding subject ID
-COL_CONDITION <- "ExpCond"  # name of the variable encoding group membership
+COL_ID <- "id"         # name of the variable encoding subject ID
+COL_CONDITION <- "exp"  # name of the variable encoding group membership
 
 # NOTE: experimental condition should be labeled meaningfully!
-unique(data[,COL_CONDITION])
-data[,COL_CONDITION] <- factor(data[,COL_CONDITION],
-                               levels = c(1:2),
-                               labels = c("no_load", "load"))
+# unique(data[,COL_CONDITION])
+# data[,COL_CONDITION] <- factor(data[,COL_CONDITION],
+#                                levels = c(1:2),
+#                                labels = c("no_load", "load"))
 
 ### check input data frame
 head(data)
@@ -87,7 +88,8 @@ head(data)
 
 TREEBUGS_MCMC <- c(n.chain = 4, n.iter = 50000, n.adapt = 3000, 
                    n.burnin = 2000, n.thin = 10,
-                   Rhat_max = 1.05, Neff_min = 100,  # = stopping criteria for MCMC sampling
+                   # = stopping criteria for MCMC:
+                   Rhat_max = 1.05, Neff_min = 100, extend_max = 20,
                    n.PPP = 2000, nCPU = 8)
 
 MPTINR_OPTIONS <- c(bootstrap_samples = 500,
@@ -99,10 +101,10 @@ MAX_CI_INDIV <- 0.99
 
 ### different settings for testing (faster, but inaccurate results!)
 # MPTINR_OPTIONS <- c(bootstrap_samples = 10, n.optim = 2, nCPU = 8)
-# TREEBUGS_MCMC <- c(n.chain = 4, n.iter = 2000, n.adapt = 100,
-#                    n.burnin = 100, n.thin = 5,
-#                    Rhat_max = 5, Neff_min = 5,
-#                    n.PPP = 100, nCPU = 8)
+# TREEBUGS_MCMC <- c(n.chain = 4, n.iter = 2000, n.adapt = 500,
+#                    n.burnin = 100, n.thin = 2,
+#                    Rhat_max = 10, Neff_min = 10, extend_max = 2,
+#                    n.PPP = 10, nCPU = 8)
 
 
 ##############################
@@ -121,5 +123,6 @@ results <- bind_rows(res_mptinr, res_treebugs)
 results
 
 # requires a subfolder "/results"
-save(results, file = paste0("results/", EQN_FILE, "-", DATA_FILE, ".RData"))
-source("4_summary_plots.R")
+save(results, file = paste0(EQN_FILE, "-", DATA_FILE, ".RData"))
+
+plot_results(results)
---
+++ kuhlmann2017/1_main_script.R
@@ -0,0 +1,126 @@
+#############################################
+## Install developer version of TreeBUGS   ##
+#############################################
+
+### the newest TreeBUGS needs to be installed once using one of the following 3 options:
+
+### (A) on Windows:
+# install.packages(c("runjags", "hypergeo","rjags", "logspline","RcppArmadillo"))
+# install.packages("../scripts/TreeBUGS_1.2.0.zip", repos = NULL)
+
+### (B) on Mac:
+# install.packages(c("runjags", "hypergeo","rjags", "logspline","RcppArmadillo"))
+# install.packages("../scripts/TreeBUGS_1.2.0.XXX", repos = NULL)
+
+### (C) compile source package (requires tools for building C++ packages)
+# install.packages(c("devtools", "runjags", "hypergeo","rjags", "logspline","RcppArmadillo"))
+# library("devtools")
+# devtools::install_github("denis-arnold/TreeBUGS")
+
+
+#################################
+## Load packages and scripts   ##
+#################################
+
+# load packages
+library("tidyr")
+library("dplyr")
+library("tibble")
+library("rlang")
+library("reshape2")
+library("ggplot2")
+library("parallel")
+library("MPTinR")
+library("TreeBUGS")
+library("runjags")
+runjags.options(silent.jags = TRUE, silent.runjags = TRUE)
+
+# load scripts. make sure that working directory is correct via either:
+# - setwd() / getwd()
+# - Rstudio->Sessions->Set Working Directory->'To Source File Location'
+source("../scripts/mptinr.R")
+source("../scripts/treebugs.R")
+source("../scripts/auxiliary_functions.R")
+source("../scripts/summary_plots.R")
+
+
+#################################
+## MPT model definition & Data ##
+#################################
+
+EQN_FILE <- "2HTSM_Submodel4.eqn"
+DATA_FILE <- "Kuhlmann_dl7.csv"  
+
+
+### if .csv format uses semicolons ";" (German format)
+data <- read.csv2(DATA_FILE, fileEncoding = "UTF-8-BOM")
+### if .csv format uses commata "," (international format)
+# data <- read.csv(DATA_FILE, fileEncoding = "UTF-8-BOM")
+head(data)
+plotFreq(data, boxplot = FALSE, eqn = "2htm.eqn")
+
+
+### optional: add a person identifier if missing in data
+# data$Subject <- 1:nrow(data)
+### optional: add a dummy variable if data do not contain a between-subject factor
+# data$ExpCond <- "no_condition"
+
+
+COL_ID <- "Subject"         # name of the variable encoding subject ID
+COL_CONDITION <- "ExpCond"  # name of the variable encoding group membership
+
+# NOTE: experimental condition should be labeled meaningfully!
+unique(data[,COL_CONDITION])
+data[,COL_CONDITION] <- factor(data[,COL_CONDITION],
+                               levels = c(1:2),
+                               labels = c("no_load", "load"))
+
+### check input data frame
+head(data)
+
+
+#################################
+## Settings                    ##
+#################################
+
+
+TREEBUGS_MCMC <- c(n.chain = 4, n.iter = 50000, n.adapt = 3000, 
+                   n.burnin = 2000, n.thin = 10,
+                   # = stopping criteria for MCMC:
+                   Rhat_max = 1.05, Neff_min = 100, extend_max = 20,
+                   n.PPP = 2000, nCPU = 8)
+
+MPTINR_OPTIONS <- c(bootstrap_samples = 500,
+                    n.optim = 10,
+                    nCPU = 8)
+
+CI_SIZE <- c(0.025, 0.1, 0.9, 0.975)
+MAX_CI_INDIV <- 0.99
+
+### different settings for testing (faster, but inaccurate results!)
+# MPTINR_OPTIONS <- c(bootstrap_samples = 10, n.optim = 2, nCPU = 8)
+# TREEBUGS_MCMC <- c(n.chain = 4, n.iter = 1000, n.adapt = 500,
+#                    n.burnin = 100, n.thin = 2,
+#                    Rhat_max = 10, Neff_min = 3, extend_max = 2,
+#                    n.PPP = 10, nCPU = 8)
+
+
+##############################
+## Analysis (do not change) ##
+##############################
+
+res_mptinr <- mpt_mptinr(dataset = DATA_FILE, data = data, model = EQN_FILE,
+                         col_id = COL_ID, col_condition = COL_CONDITION)
+
+res_treebugs <- lapply(c("simple", "simple_pooling", "trait", "beta"),
+                       FUN = mpt_treebugs, 
+                       dataset = DATA_FILE, data = data, model = EQN_FILE,
+                       col_id = COL_ID, col_condition = COL_CONDITION)
+
+results <- bind_rows(res_mptinr, res_treebugs)
+results
+
+# requires a subfolder "/results"
+save(results, file = paste0(EQN_FILE, "-", DATA_FILE, ".RData"))
+
+plot_results(results)
